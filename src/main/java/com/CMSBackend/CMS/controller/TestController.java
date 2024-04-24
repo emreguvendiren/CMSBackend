f18d163887671e1bbb3b4df0f9eb2e4e98192f28
emreguvendiren <guvendirenemre1@gmail.com>
2024-04-24 22:31:28 +0300

    First App

---
+++ src/main/java/com/CMSBackend/CMS/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.CMSBackend.CMS.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.CMSBackend.CMS.dto.ResultDto;
+
+@RestController
+@RequestMapping("/test")
+@CrossOrigin
+public class TestController {
+	
+	@GetMapping("/hello")
+	public ResultDto helloWorld() {
+		return new ResultDto(true,200,"Hello World");
+		//
+	}
+
+}
---
+++ src/main/java/com/CMSBackend/CMS/dto/ResultDto.java
@@ -0,0 +1,37 @@
+package com.CMSBackend.CMS.dto;
+
+public class ResultDto {
+
+	public Object result;
+	public int status;
+	public String message;
+	
+	
+	public ResultDto(Object result,int status,String message) {
+		this.result = result;
+		this.status = status;
+		this.message = message;
+	}
+	
+	public Object getResult() {
+		return result;
+	}
+	public void setResult(Object result) {
+		this.result = result;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+	
+}
