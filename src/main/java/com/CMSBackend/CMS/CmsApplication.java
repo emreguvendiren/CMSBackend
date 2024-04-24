4905a98cb298ae14cdc7d7c0b1c8acd67508aa38
FURKAN İÇÖZ <furkanicoz@outlook.com>
2024-04-25 00:28:46 +0300

    gfjsfg

--- src/main/java/com/CMSBackend/CMS/CmsApplication.java
+++ src/main/java/com/CMSBackend/CMS/CmsApplication.java
@@ -2,8 +2,7 @@ package com.CMSBackend.CMS;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.stereotype.Component;
+
 
 @SpringBootApplication
 public class CmsApplication {
