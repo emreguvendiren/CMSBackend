a03aabbf0c3b8341f3b267c2caf0c353e417bf48
emreguvendiren <guvendirenemre1@gmail.com>
2024-10-15 21:52:19 +0300

    Base Entity User Log

--- src/main/java/com/CMSBackend/CMS/model/BaseEntity.java
+++ src/main/java/com/CMSBackend/CMS/model/BaseEntity.java
@@ -3,10 +3,15 @@ package com.CMSBackend.CMS.model;
 
 import java.util.Date;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.security.core.context.SecurityContextHolder;
+
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
 
 @MappedSuperclass
 public class BaseEntity {
@@ -15,7 +20,18 @@ public class BaseEntity {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	public Long id;
 	public Date createDate;
+
 	public String createrUser;
+	
+	
+	@PrePersist
+    protected void onCreate() {
+    	try {
+    		createrUser = SecurityContextHolder.getContext().getAuthentication().getName();
+		} catch (Exception e) {
+			
+		}
+    }
 	
 	public BaseEntity() {
 		setCreateDate(new Date());
